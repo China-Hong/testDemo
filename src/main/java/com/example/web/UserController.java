2e7b91184114cf489f644b62d5cdfaad3435a468
guoyuanhong <guoyuanhong789@163.com>
2019-12-19 10:11:12 +0800

    冲突

--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -54,12 +54,13 @@ public class UserController {
         //test.test2();
         // 处理"/users/"的GET请求，用来获取用户列表
         // 还可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
-        List<User> r = new ArrayList<User>(users.values());
-        Message message=MessageBuilder.withBody(objectMapper.writeValueAsBytes(r.get(0))).setDeliveryMode(MessageDeliveryMode.PERSISTENT).build();
+        List<User> rs = new ArrayList<User>(users.values());
+        Message message=MessageBuilder.withBody(objectMapper.writeValueAsBytes(rs.get(0))).setDeliveryMode(MessageDeliveryMode.PERSISTENT).build();
         message.getMessageProperties().setHeader(AbstractJavaTypeMapper.DEFAULT_CONTENT_CLASSID_FIELD_NAME, MessageProperties.CONTENT_TYPE_JSON);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         rabbitTemplate.convertAndSend("direct",message);
-        return r;
+        System.out.println("");
+        return rs;
     }
 
     @ApiOperation(value="创建用户", notes="根据User对象创建用户")
